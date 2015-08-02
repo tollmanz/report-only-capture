bdccb7a0f4c5a80971cc9565d248c0634340fa53
Zack Tollman <tollmanz@gmail.com>
2015-08-02 16:08:55 -0700

    Cleanup browsers to only add ones that work.

--- automate.js
+++ automate.js
@@ -11,65 +11,6 @@ if (user === '' || pass === '') {
 }
 
 var browsers = [
-  //No reporting
-  {
-    'browser' : 'IE',
-    'browser_version' : '11.0',
-    'os' : 'Windows',
-    'os_version' : '8.1',
-    'resolution' : '1600x1200'
-  },
-  {
-    'browser' : 'IE',
-    'browser_version' : '10.0',
-    'os' : 'Windows',
-    'os_version' : '8',
-    'resolution' : '1600x1200'
-  },
-  {
-    'browser' : 'IE',
-    'browser_version' : '9.0',
-    'os' : 'Windows',
-    'os_version' : '7',
-    'resolution' : '1600x1200'
-  },
-  {
-    'browser' : 'Safari',
-    'browser_version' : '8.0',
-    'os' : 'OS X',
-    'os_version' : 'Yosemite',
-    'resolution' : '1600x1200'
-  },
-  {
-    'browser' : 'Safari',
-    'browser_version' : '7.0',
-    'os' : 'OS X',
-    'os_version' : 'Mavericks',
-    'resolution' : '1600x1200'
-  },
-  {
-    'browser' : 'Safari',
-    'browser_version' : '6.1',
-    'os' : 'OS X',
-    'os_version' : 'Mountain Lion',
-    'resolution' : '1600x1200'
-  },
-  {
-    'browser' : 'Safari',
-    'browser_version' : '6.0',
-    'os' : 'OS X',
-    'os_version' : 'Lion',
-    'resolution' : '1600x1200'
-  },
-  {
-    'browser' : 'Safari',
-    'browser_version' : '5.1',
-    'os' : 'OS X',
-    'os_version' : 'Snow Leopard',
-    'resolution' : '1600x1200'
-  },
-
-  // Reporting
   {
     'browser' : 'Chrome',
     'browser_version' : '44.0',
@@ -176,88 +117,6 @@ var browsers = [
     'resolution' : '1024x768',
   },
   {
-    'browser' : 'Firefox',
-    'browser_version' : '39.0',
-    'os' : 'Windows',
-    'os_version' : '8.1',
-    'resolution' : '1024x768'
-  },
-  {
-    'browser' : 'Firefox',
-    'browser_version' : '38.0',
-    'os' : 'OS X',
-    'os_version' : 'Yosemite',
-    'resolution' : '1600x1200'
-  },
-  {
-    'browser' : 'Firefox',
-    'browser_version' : '37.0',
-    'os' : 'OS X',
-    'os_version' : 'Yosemite',
-    'resolution' : '1600x1200'
-  },
-  {
-    'browser' : 'Firefox',
-    'browser_version' : '36.0',
-    'os' : 'OS X',
-    'os_version' : 'Yosemite',
-    'resolution' : '1600x1200'
-  },
-  {
-    'browser' : 'Firefox',
-    'browser_version' : '35.0',
-    'os' : 'OS X',
-    'os_version' : 'Yosemite',
-    'resolution' : '1600x1200'
-  },
-  {
-    'browser' : 'Firefox',
-    'browser_version' : '34.0',
-    'os' : 'OS X',
-    'os_version' : 'Yosemite',
-    'resolution' : '1600x1200'
-  },
-  {
-    'browser' : 'Firefox',
-    'browser_version' : '33.0',
-    'os' : 'OS X',
-    'os_version' : 'Yosemite',
-    'resolution' : '1600x1200'
-  },
-  {
-    'browser' : 'Firefox',
-    'browser_version' : '32.0',
-    'os' : 'OS X',
-    'os_version' : 'Yosemite',
-    'resolution' : '1600x1200'
-  },
-  {
-    'browser' : 'Firefox',
-    'browser_version' : '31.0',
-    'os' : 'OS X',
-    'os_version' : 'Yosemite',
-    'resolution' : '1600x1200'
-  },
-  {
-    'browser' : 'Firefox',
-    'browser_version' : '30.0',
-    'os' : 'OS X',
-    'os_version' : 'Yosemite',
-    'resolution' : '1600x1200'
-  },
-  {
-    'browserName' : 'iPhone',
-    'platform' : 'MAC',
-    'device' : 'iPhone 5S',
-    'version' : '7.0'
-  },
-  {
-    'browserName' : 'iPhone',
-    'platform' : 'MAC',
-    'device' : 'iPhone 5',
-    'version' : '6.1'
-  },
-  {
     'browser' : 'Opera',
     'browser_version' : '12.16',
     'os' : 'Windows',
@@ -306,18 +165,6 @@ var browsers = [
     'platform' : 'ANDROID',
     'device' : 'Google Nexus',
     'version' : '4.0'
-  },
-  {
-    'browserName' : 'android',
-    'platform' : 'ANDROID',
-    'device' : 'Samsung Galaxy Note',
-    'version' : '2.3'
-  },
-  {
-    'browserName' : 'android',
-    'platform' : 'ANDROID',
-    'device' : 'Galaxy S',
-    'version' : '2.2'
   }
 ];
 
