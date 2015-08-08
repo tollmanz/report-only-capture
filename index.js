e18990dcfce307815d5bda7012c14bf36c3f0613
Zack Tollman <tollmanz@gmail.com>
2015-08-08 08:28:17 -0700

    Merge test page and collector into one script.

--- index.js
+++ index.js
@@ -1,78 +1,145 @@
-var cluster = require('cluster');
+var Hapi = require('hapi');
+var Path = require('path');
+var querystring = require('querystring');
+var handlebars = require('handlebars');
+
 var host = (process.argv[2]) ? process.argv[2] : 'localhost';
-var bucketName = (process.argv[3]) ? process.argv[3] : 'default';
-var port = (process.argv[4]) ? process.argv[4] : 8000;
+var port = (process.argv[3]) ? process.argv[3] : '8123';
+var bucketName = (process.argv[4]) ? process.argv[4] : 'default';
+var cspPort = (process.argv[5]) ? process.argv[5] : port;
+var cspUrl = 'http://' + host + ':' + cspPort + '/csp-report';
 
-// Code to run if we're in the master process
-if (cluster.isMaster) {
+var baseDirective = " 'self' https:";
 
-  // Count the machine's CPUs
-  var cpuCount = require('os').cpus().length;
+var directivesNames = [
+  'default-src',
+  'child-src',
+  'connect-src',
+  'font-src',
+  'img-src',
+  'media-src',
+  'object-src',
+  'script-src',
+  'style-src',
+  'form-action'
+];
 
-  // Create a worker for each CPU
-  for (var i = 0; i < cpuCount; i += 1) {
-      cluster.fork();
+var directiveString = directivesNames.join(' ' + baseDirective + '; ');
+directiveString += ' ' + baseDirective + "; frame-ancestors 'none'; plugin-types 'none';";
+
+var server = new Hapi.Server();
+server.connection({
+  host: host,
+  port: port,
+  labels: ['default']
+});
+
+server.views({
+    engines: {
+        html: require('handlebars')
+    },
+    path: Path.join(__dirname, 'templates')
+});
+
+// Configure the cookie
+server.state('snickerdoodle', {
+  ttl: null,
+  isSecure: false,
+  isHttpOnly: true,
+  encoding: 'none',
+  clearInvalid: true,
+  strictHeader: true // don't allow violations of RFC 6265
+});
+
+// Add the index route
+server.route({
+  method: 'GET',
+  path: '/',
+  config: {
+    handler: function(request, reply) {
+      reply
+        .view('index')
+        .state('snickerdoodle', 'cinnamon');
+    }
+  }
+});
+
+// Add the route
+server.route({
+  method: 'GET',
+  path:'/csp',
+  config: {
+    handler: function (request, reply) {
+      var getVars = querystring.stringify(request.query);
+      var url = (getVars !== '') ? cspUrl + '?' + getVars : cspUrl;
+
+      reply
+        .view('csp')
+        .header(
+          'Content-Security-Policy',
+          directiveString + ' report-uri ' + url
+        )
+        .header(
+          'X-Content-Security-Policy',
+          directiveString + ' report-uri ' + url
+        )
+        .header(
+          'X-Webkit-CSP',
+          directiveString + ' report-uri ' + url
+        );
+    }
+  }
+});
+
+// Add the CSP collector
+if (port !== cspPort) {
+  // Add another port to listen on if needed
+  server.connection({
+    port: cspPort,
+    labels: ['csp-report']
+  });
+}
+
+server.ext('onRequest', function(request, reply) {
+  if ('application/csp-report' === request.headers['content-type']) {
+    request.headers['content-type'] = 'application/json';
+    request.headers['x-content-type'] = 'application/csp-report';
   }
 
-  // Listen for dying workers
-  cluster.on('exit', function (worker) {
+  return reply.continue();
+});
 
-      // Replace the dead worker, we're not sentimental
-      console.log('Worker ' + worker.id + ' died :(');
-      cluster.fork();
+// Add the route
+server.route({
+  method: 'POST',
+  path:'/csp-report',
+  config: {
+    handler: function (request, reply) {
+      var date = new Date().getTime();
 
-  });
+      var data = {
+        query: request.query,
+        date: date,
+        header: request.headers,
+        body: request.payload
+      };
 
-// Code to run if we're in a worker process
-} else {
-  var Hapi = require('hapi');
-  var hash = require('object-hash');
-  var couchbase = require('couchbase');
-  var cluster = new couchbase.Cluster('couchbase://127.0.0.1');
-  var bucket = cluster.openBucket(bucketName);
+      var couchbase = require('couchbase');
+      var cluster = new couchbase.Cluster('couchbase://127.0.0.1');
+      var bucket = cluster.openBucket(bucketName);
 
-  var server = new Hapi.Server();
-  server.connection({
-    host: host,
-    port: port
-  });
-
-  server.ext('onRequest', function(request, reply) {
-    if ('application/csp-report' === request.headers['content-type']) {
-      request.headers['content-type'] = 'application/json';
-      request.headers['x-content-type'] = 'application/csp-report';
+      bucket.upsert(date.toString(), data, function(err, result) {
+        if (err) {
+          throw err;
+        } else {
+          bucket.disconnect();
+          console.log(data);
+          reply( data );
+        }
+      });
     }
+  }
+});
 
-    return reply.continue();
-  });
-
-  // Add the route
-  server.route({
-    method: 'POST',
-    path:'/csp-report',
-    config: {
-      handler: function (request, reply) {
-        var date = new Date().getTime();
-
-        var data = {
-          query: request.query,
-          date: date,
-          header: request.headers,
-          body: request.payload
-        };
-
-        bucket.upsert(date.toString(), data, function(err, result) {
-          if (err) {
-            throw err;
-          } else {
-            reply( data );
-            console.log(data);
-          }
-        });
-      }
-    }
-  });
-
-  // Start the server
-  server.start();
-}
+// Start the server
+server.start();
